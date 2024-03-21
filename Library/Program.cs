2749bab3ade1aaca4980fdf91e33946603ba076d
samuelkamotho92 <samuelkamotho92@gmail.com>
2024-03-21 23:01:18 +0300

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,37 @@
+using Library.Data;
+using Library.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBookService, BookService>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SQLServerConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
